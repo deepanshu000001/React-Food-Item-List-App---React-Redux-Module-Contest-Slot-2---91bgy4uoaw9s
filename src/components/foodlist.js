d5495dac5596e1d5ba0854973ea5cf8687b41d12
deepanshu000001 <143742351+deepanshu000001@users.noreply.github.com>
2024-02-11 18:19:16 +0530

    Update foodlist.js

--- src/components/foodlist.js
+++ src/components/foodlist.js
@@ -10,48 +10,96 @@ function FoodList() {
 	const [isSecondCardEnabled, setIsSecondCardEnabled] = useState(false);
 	const [isFormEnabled, setIsFormEnabled] = useState(false);
 
+	function handleSave(){
+
+		let id = Math.round(Math.random()*100);
+
+		setFoods(prev=>{
+			
+			return [...prev,{id,itemName,foodType,spicinessLevel}]
+		});
+
+		setIsFirstCardEnabled(false);
+		setIsSecondCardEnabled(false);
+		setIsFormEnabled(false)
+		setItemName("");
+		setFoodType("");
+		setSpicinessLevel("");
+	}
+
+	function handleDelete(id){
+
+		setFoods(prev=>{
+
+			return prev.filter(pre=>pre.id != id);
+
+		})
+
+	}
+
 	return (
 		<>
 			<div className="container">
 				<h1>Food Items List</h1>
-				<button>Add Food</button>
+				<button onClick={()=>{
+					setIsFirstCardEnabled(true);
+					setIsSecondCardEnabled(true);
+				}}>Add Food</button>
 
-				<div className="card-container">
+				{isFirstCardEnabled && <div className="card-container">
                         <>
 							<h2>Item Name:</h2>
 							<input
 								name="itemName"
 								type="text"
 								disabled={!isFirstCardEnabled}
+								value={itemName}
+								onChange={(e)=>{
+									setItemName(e.target.value)
+								}}
 							/>
 							<h2>Food Type:</h2>
 							<input
 								name="foodType"
 								type="text"
 								disabled={!isFirstCardEnabled}
+								value={foodType}
+								onChange={(e)=>{
+									setFoodType(e.target.value)
+								}}
 							/>
 							<div className={`card`}>
-								<form>
+								<form style={{opacity: isFormEnabled ? 1 : 0.5 }} onClick={()=>{
+									setIsFormEnabled(true);
+								}}>
 									<h2>Spiciness Level:</h2>
 									<input
 										name="spicinessLevel"
 										type="text"
 										disabled={!isFormEnabled}
+										value={spicinessLevel}
+										onChange={(e)=>{
+											setSpicinessLevel(e.target.value)
+											}}
 									/>
 								</form>
 							</div>
 						</>
-				</div>
+				</div>}
                 <div className={`card ${isSecondCardEnabled ? "" : "disabled"}`}>
-						<button>Save</button>
+						<button onClick={handleSave}>Save</button>
 				</div>
 
-				<ul className="list">
-                        <li>
-							{itemName} ({foodType}) - Spiciness Level:{" "}
-							{spicinessLevel}
-							<button>Delete</button>
-						</li>
+				 <ul className="list">
+                       {foods?.map(food=>(
+						 <li key={food?.id}>
+						 {food?.itemName} ({food?.foodType}) - Spiciness Level:{" "}
+						 {food?.spicinessLevel}
+						 <button onClick={()=>{
+							handleDelete(food?.id);
+						 }} >Delete</button>
+					 </li>
+					   ))}
 				</ul>
 			</div>
 		</>
